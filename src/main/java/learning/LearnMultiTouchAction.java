9385728ad66f46f705e605c20c21f982f11dbfaa
Thyagarajan <lokeshkumar.thyagarajan@hidglobal.com>
2023-01-21 11:05:12 +0530

    Day 3 code added

--- src/main/java/learning/LearnMultiTouchAction.java
+++ src/main/java/learning/LearnMultiTouchAction.java
@@ -1,17 +1,17 @@
 package learning;
 
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.time.Duration;
-
-import org.openqa.selenium.remote.DesiredCapabilities;
-
+import io.appium.java_client.MultiTouchAction;
 import io.appium.java_client.TouchAction;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.touch.WaitOptions;
 import io.appium.java_client.touch.offset.PointOption;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
-public class LearnTouchAction {
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.time.Duration;
+
+public class LearnMultiTouchAction {
 	public static void main(String[] args) throws MalformedURLException, InterruptedException {
 		DesiredCapabilities dc = new DesiredCapabilities();
 		dc.setCapability("appium:app", System.getProperty("user.dir") + "/apks/MultiTouch Tester_v1.2.apk");
@@ -29,10 +29,16 @@ public class LearnTouchAction {
 		int startY = (int) (maxY * 0.8);
 		int endX = (int) (maxX * 0.5);
 		int endY = (int) (maxY * 0.2);
-		new TouchAction<>(driver).press(PointOption.point(startX, startY))
+
+		new MultiTouchAction(driver)
+				.add(new TouchAction<>(driver).press(PointOption.point(startX, startY))
 				.waitAction(WaitOptions.waitOptions(Duration.ofMillis(2000)))
 				.moveTo(PointOption.point(endX, endY))
-				.release().perform();
+				.release())
+				.add(new TouchAction<>(driver).press(PointOption.point(endX, endY))
+				.waitAction(WaitOptions.waitOptions(Duration.ofMillis(2000)))
+				.moveTo(PointOption.point(startX, startY))
+				.release()).perform();
 		driver.quit();
 
 	}
